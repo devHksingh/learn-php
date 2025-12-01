1e7caebccafbd6a9e1b5680a1e25431ade2c6a17
devHksingh <hksingh.dev@gmail.com>
2025-12-01 19:56:11 +0530

    Add initial PHP expressions example

---
+++ basic/expressions.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "hello php\n";
+echo "welcome\n";
+
+$name = "Alice";
+
+echo "Hello ". $name .  "!\n";
+
+$pizza = 4;
+$slicePerPizza = 8;
+
+$totalSlices = $pizza * $slicePerPizza;
+echo "Total pizza slice: " . $totalSlices . "\n";
+
+$isHunggry = true;
+echo "Hungry?\n";
+echo $isHunggry? "Yes\n":"No\n";
+
+/**
+ * variable 
+ * start with $ then variable name use "_" underscore for gap.not to  php reversed name
+ */
